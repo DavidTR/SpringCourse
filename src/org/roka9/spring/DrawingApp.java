48d63b6faf02c8edfd9702381b253bf995aa248b
David Téllez Rodríguez <davtelrod@gmail.com>
2016-10-09 01:36:12 +0200

    Fin del primer bloque del tutorial

--- src/org/roka9/spring/DrawingApp.java
+++ src/org/roka9/spring/DrawingApp.java
@@ -1,5 +1,7 @@
 package org.roka9.spring;
 
+// Repositorio para el curso: https://javabrains.io/courses/spring_core
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
@@ -11,7 +13,6 @@ import org.springframework.core.io.FileSystemResource;
 public class DrawingApp {
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Triangle triangle = new Triangle();
 		
 		// 1. BeanFactory. La fábrica de Beans, forma básica de hacerlo.
@@ -23,8 +24,28 @@ public class DrawingApp {
 		// usará el anterior sobre éste si tenemos un problema de escasez de memoria, por ejemplo 
 		// (applets).
 		ApplicationContext context = new FileSystemXmlApplicationContext("spring.xml");
-		Triangle triangle = (Triangle) context.getBean("triangle");
 		
-		triangle.draw();
+		// Tipos de triángulo: Se diferencian en la complejidad de su construcción.
+		// 1. Triángulo básico con inyección de dependencias de tipos primitivos (int, string).
+		Triangle basicTriangle = (Triangle) context.getBean("basicTriangle");
+		basicTriangle.draw("basicTriangle");
+
+		// 2. Triángulo construido con inyección de dependencias a otros objetos creados también en spring.
+		Triangle pointTriangle = (Triangle) context.getBean("pointTriangle");
+		pointTriangle.draw("pointTriangle");
+
+		// 3. Triángulo construido con inyección de dependencias a otros objetos creados también en spring, con dependencias AdHoc (específicas de este objeto).
+		// pointTAH es un alias para pointTriangleAdHoc, se pueden usar ambos nombres. Recomendable usar IDs.
+		Triangle pointTriangleAdHoc = (Triangle) context.getBean("pointTAH");
+		pointTriangleAdHoc.draw("pointTriangleAdHoc");
+		
+		// 4. Triángulo construido con inyección de dependencias a otros objetos creados también en spring, con dependencias en lista.
+		Triangle pointTriangleList = (Triangle) context.getBean("pointTriangleList");
+		pointTriangleList.draw("pointTriangleList");
+		
+		// 5. Triángulo construido con inyección de dependencias a otros objetos creados también en spring, con autowire.
+		Triangle pointTriangleAutoWire = (Triangle) context.getBean("pointTriangleAutoWire");
+		pointTriangleAutoWire.draw("pointTriangleAutoWire");
+		
 	}
 }
--- src/org/roka9/spring/Triangle.java
+++ src/org/roka9/spring/Triangle.java
@@ -1,19 +1,105 @@
 package org.roka9.spring;
 
+import java.util.List;
+
 public class Triangle {
 	
+	// 1. Inyección de dependencias (valor para type) por parámetro en setter.
 	private String type;
 	
-	public void draw() {
-		System.out.println("Triangle of type " + getType() + " drawn.");
+	// 2. Inyección de dependencias por parámetro a constructor.
+	private Integer height;
+	
+	// 3. Inyección de dependencias de objetos completos, no de tipos primitivos.
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+	
+	// 4. Inyección de dependencias de listas de objetos completos.
+	private List<Point> points;
+
+	public Integer getHeight() {
+		return height;
+	}
+
+	// 2. Inyección de dependencias por parámetro a constructor.
+	public Triangle (String type) {
+		this.type = type;
+	}
+	
+	public Triangle (String type, Integer height) {
+		this.type = type;
+		this.height = height;
+	}
+	
+	public Triangle (Integer height) {
+		this.height = height;
+	}
+	
+	public void draw(String triangleName) {
+		
+		String pointsInfo = ".";
+		
+		// Si todos los puntos tienen valor, mostrarlos por pantalla.
+		
+		// 1. Puntos individuales.
+		if (pointA != null && pointB != null && pointC != null) {
+			pointsInfo = ", coordinates are: Point A: " + pointA.getX() + " - " + pointA.getY() + 
+									      ", Point B: " + pointB.getX() + " - " + pointB.getY() +
+									      ", Point C: " + pointC.getX() + " - " + pointC.getY() + ".";
+		}
+		
+		// 2. Lista de puntos.
+		if (points != null) {
+			pointsInfo = ", coordinates are: Point A: " + points.get(0).getX() + " - " + points.get(0).getY() + 
+										  ", Point B: " + points.get(1).getX() + " - " + points.get(1).getY() +
+										  ", Point C: " + points.get(2).getX() + " - " + points.get(2).getY() + ".";
+		}
+		
+		System.out.println("The triangle " + triangleName + ", of type " + getType() + " and height " + getHeight() + " has been drawn" + pointsInfo);
 		
 	}
 
 	public String getType() {
 		return type;
 	}
+	
 
-	public void setType(String type) {
-		this.type = type;
+	public Point getPointA() {
+		return pointA;
 	}
+
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	
+	public List<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(List<Point> points) {
+		this.points = points;
+	}
+	
+	// 1. Inyección de dependencias (valor para type) por parámetro en setter.
+/*	public void setType(String type) {
+		this.type = type;
+	}*/
 }
